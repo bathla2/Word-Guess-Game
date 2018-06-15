dcc4366011e81ea4469d7d530eeee5474f7c476b
bathla2 <39197587+bathla2@users.noreply.github.com>
2018-06-15 00:30:41 -0400

    Update game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,12 +3,34 @@ var userText = document.getElementById("user-text");
 
 var textOne = document.getElementById("text1");
 var wrongLetter = document.getElementById("text2");
+var win = document.getElementById("text3");
+var loss = document.getElementById("text4");
+var win1 = 0;
+var loss1 = 0;
 var computerChoices = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
 var word = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 var remainingLetters=word.length;
 var chances = 10;
 var answer = [];
 var wrongLetters = [];
+
+function resetGame() {
+  textOne = document.getElementById("text1");
+  wrongLetter.textContent = " ";
+  textOne.textContent = "10";
+  computerChoices = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
+  word = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+  remainingLetters=word.length;
+  chances = 10;
+  answer = [];
+  wrongLetters = [];
+  for(var i=0; i<word.length; i++)
+     {
+      answer[i] = "_";
+     }
+     userText.textContent = answer.join(",");
+  
+ }
     for(var i=0; i<word.length; i++)
      {
       answer[i] = "_";
@@ -59,13 +81,17 @@ var check = false;
  if (chances<1)
   {
      alert("Thanks for playing, please try again. The correct word was: " + word);
-     location.reload();
+     loss1 = loss1+1;
+     loss.textContent=(loss1);
+     resetGame();
      
   }
   else if (remainingLetters<=0)
   {
     alert ("Congratulations, you guessed the correct word, the answer is: " + word);
-    location.reload();
+    win1 = win1 + 1;
+    win.textContent=(win1);
+    resetGame();
     
   }
 };
